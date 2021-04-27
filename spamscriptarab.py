56a2b62f3d38204cafb17b133db9233b44ae9d02
Lorem <81118866+MrJakeSir@users.noreply.github.com>
2021-04-26 19:24:58 -0600

    Value error bug fixed

--- spamscriptarab.py
+++ spamscriptarab.py
@@ -3,6 +3,7 @@ import pyperclip
 import keyboard
 import random
 import colorama
+# from gencode import Gencode
 
 print("""
     _                  _    _         _
@@ -12,11 +13,11 @@ print("""
     |_|\_\__,_/___\__,_|_|\_\_| |_|___/\__\__,_|_| |_|
 """, colorama.Fore.RED,
 """
- _                     _
-| |__   ___  _ __ ___ | |__
-| '_ \ / _ \| '_ ` _ \| '_ \\
-| |_) | (_) | | | | | | |_) |
-|_.__/ \___/|_| |_| |_|_.__/
+    _                     _
+    | |__   ___  _ __ ___ | |__
+    | '_ \ / _ \| '_ ` _ \| '_ \\
+    | |_) | (_) | | | | | | |_) |
+    |_.__/ \___/|_| |_| |_|_.__/
 """)
 stringArab = """
 ضصثقفغعهخحجدذشسيبلاتنمكط\ئءؤرىةوزظ
@@ -43,51 +44,98 @@ print(colorama.Fore.LIGHTRED_EX,"""
 print(colorama.Fore.LIGHTGREEN_EX,
       "[*]", colorama.Style.RESET_ALL,
       "Input the number of messages you wanna bomb with")
-election = int(input("->    "))
+try:
+    election = int(input("->    "))
+except ValueError:
+    state = False
+    print("Wroooong, type a number")
+    while state == False:
+        try:
+            election = int(input("->    "))
+            state = True
+            break
+        except ValueError:
+            continue
+        
+        
 print(colorama.Fore.LIGHTGREEN_EX,
       "[*]", colorama.Style.RESET_ALL,
       "Input the cooldown you want to have")
-election_cld = int(input("->    "))
+try:
+    election_cld = float(input("->    "))
+except ValueError:
+    state = False
+    print("Wroooong, type a number")
+    while state == False:
+        try:
+            election_cld = float(input("->    "))
+            state = True
+            break
+        except ValueError:
+            continue
+        
+        
 
+print(colorama.Fore.LIGHTGREEN_EX,
+      "[*]", colorama.Style.RESET_ALL,
+      "Input how faster you want the spam to be \n",
+      "Example: 0, 0.5, 1, 1.5")
+try:
+    election_cld_msg = float(input("->    "))
+except ValueError:
+    state = False
+    print("Wroooong, type a number")
+    while state == False:
+        try:
+            election_cld_msg = float(input("->    "))
+            state = True
+            break
+        except ValueError:
+            continue
+        
+        
+pyautogui.PAUSE = election_cld_msg
 
+print(colorama.Fore.LIGHTRED_EX, "[*] ",
+      "You're about to run this program,\n this program will take FULL control of your keyboard, you'll have\n",
+      f"{election_cld} seconds to go to the application you want to bomb, are you SURE you want to continue?",
+      "\n (N,y)")
 
-
-# This line is IMPORTANT, it will define how faster the spam will be
-# Since this is open source, you can modify this as you want
-
-"""Examples"""
-"""anti discord cooldown"""
-# pyautogui.PAUSE = 1.5
-"""ABSOLUTELY INSANE spam""" 
-# pyautogui.PAUSE = 0
-# Just modify it! its completely free dude!
-
-pyautogui.PAUSE = 0.028
-
-
-for i in range(election_cld):
+submit = input("->    ")
+if submit.lower() == "n":
+    exit()
+elif submit.lower() == "y":
+    print("Ok, if you want to cancel the program, you can do it pressing ctrl + c",
+          "When it is spamming, you'll have to press the key '1'.")
+while submit.lower() != "y" and submit.lower() != "n":
+    submit = input("->    ")
+    
+for i in range(int(election_cld)):
     
     print(colorama.Fore.LIGHTGREEN_EX, "[*]", colorama.Style.RESET_ALL, f"Starting in... {election_cld} seconds..")
     election_cld -= 1
     pyautogui.sleep(1)
 print(colorama.Fore.RED, "~~~Starting...~~~", colorama.Style.RESET_ALL)
-try:
-    for i in range(election):
-        if keyboard.is_pressed('1'):
-            print(colorama.Fore.LIGHTGREEN_EX, "[*]", colorama.Style.RESET_ALL, "Successfully cancelled program [Closing...]")
-            exit()
+
+
+
+
+for i in range(election):
+    if keyboard.is_pressed('1'):
+        print(colorama.Fore.LIGHTGREEN_EX, "[*]", colorama.Style.RESET_ALL, "Successfully cancelled program [Closing...]")
+        exit()
         
-        ransel="".join(random.sample(stringArab, len(stringArab)))
-        all_str += ransel
-        all_str += warn
-        pyperclip.copy(all_str)
-        pyautogui.hotkey('ctrl', 'v')
-        pyautogui.sleep(0.014)
-        pyautogui.press('enter')
-        all_str = ""
-        count += 1
-        print(colorama.Fore.LIGHTGREEN_EX, "[*]", colorama.Style.RESET_ALL, f"Successfully sended message [{count}]")
-except ValueError:
-    print("Invalid value")
-finally:
-    print(colorama.Fore.RED, "Closing program...")
+    ransel="".join(random.sample(stringArab, len(stringArab)))
+    all_str += ransel
+    all_str += warn
+    pyperclip.copy(all_str)
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.sleep(0.014)
+    pyautogui.press('enter')
+    all_str = ""
+    count += 1
+    print(colorama.Fore.LIGHTGREEN_EX, "[*]", colorama.Style.RESET_ALL, f"Successfully sended message [{count}]")
+
+
+
+print(colorama.Fore.RED, "Closing program...")
